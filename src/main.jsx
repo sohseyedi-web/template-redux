201614580b61e4a9f082985899fed70b3b70d9e3
sohseyedi <sohseyedi@gmail.com>
2023-11-04 14:48:40 +0330

    redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import { store } from "./store/store";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,14 @@
 import { createStore } from "redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
 import { rootReducer } from "./reducer";
 
-export const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const perReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(perReducer);
